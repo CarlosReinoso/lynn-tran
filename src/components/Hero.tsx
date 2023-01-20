e142b28e3783894501595d4ca004ad80caa63e77
Carlos Reinoso <carlos.reinoso@myraindrop.co.uk>
2023-01-20 20:10:24 +0000

    test video

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Hero = () => {
   return (
-    <video autoPlay loop muted>
+    <video autoPlay loop muted style={{ width: "100%", height: "100%" }}>
       <source src="https://storage.googleapis.com/sacko-backend.appspot.com/YouCut_20211107_183543938.mp4" />
     </video>
   );
