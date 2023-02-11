fbf65a531545250b78d49f4bfb407428dcb2319e
Carlos Reinoso <carlos.reinoso@myraindrop.co.uk>
2023-02-11 17:52:55 +0000

    wip sanity/cli

--- sanity-project-lynn/sanity.cli.ts
+++ sanity-project-lynn/sanity.cli.ts
@@ -3,6 +3,6 @@ import {defineCliConfig} from 'sanity/cli'
 export default defineCliConfig({
   api: {
     projectId: 'bbm1b0xb',
-    dataset: 'production'
-  }
+    dataset: 'production',
+  },
 })
