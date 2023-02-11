37a3e62dc01ba7b2e8528520946b04a83c032202
Carlos Reinoso <carlos.reinoso@myraindrop.co.uk>
2023-02-11 12:28:17 +0000

    deployed sanity check in uat

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,24 +1,20 @@
-import { Box, Grid, Stack } from "@mui/material";
+import { Grid } from "@mui/material";
 import Image from "next/image";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import bioPhoto from "../../public/img/about/biopic.jpg";
 import { createClient } from "next-sanity";
 
 const client = createClient({
   projectId: "bbm1b0xb",
   dataset: "production",
-  // apiVersion: "2023-02-10",
   useCdn: true,
 });
 const About = () => {
+  const [text, setText] = useState("");
   useEffect(() => {
     (async () => {
-      // const about = await client.fetch(`*[_type == "about"]`);
-      // console.log("🚀 ~ file: About.tsx:18 ~ about", about);
-      const test = fetch(
-        "https://63e68c3ec8839ccc285ab552.mockapi.io/mock"
-      ).then((res) => console.log(res.json()))
-      console.log("🚀 ~ file: About.tsx:21 ~ test", test);
+      const { name } = await client.fetch(`*[_type == "about"][0]{name}`);
+      setText(name);
     })();
   });
 
@@ -38,17 +34,7 @@ const About = () => {
         <Image src={bioPhoto} alt="about" width={300} height={400} />
       </Grid>
       <Grid item xs={12} sm={12} md={6}>
-        <div>
-          Lynn Tran Energy and body worker. Innerdance facilitator Space holder
-          Although there's nothing to technically "fix" or "heal" in this now
-          moment. Healing will go on and take place anyway. Naturally occuring
-          alongside "the pause" as we journey on. Moment to moment. Particularly
-          during the depths of intentional anchoring and courageous feeling.
-          This is because "the pause" is a portal for alchemical transformation.
-          A space we touch where magic pours through. and where the
-          recalibration of our every cell takes place. That is why there's
-          nothing to fix. And only everything to feel. It's all in the pause.
-        </div>
+        <div>{text}</div>
       </Grid>
     </Grid>
   );
