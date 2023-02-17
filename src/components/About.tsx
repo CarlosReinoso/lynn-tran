0145d38f8664dd33b37771d550434c4c9369003d
Carlos Reinoso <carlos.reinoso@myraindrop.co.uk>
2023-02-17 23:12:03 +0000

    projectId env, render rich text editor and images

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -3,20 +3,60 @@ import Image from "next/image";
 import React, { useEffect, useState } from "react";
 import bioPhoto from "../../public/img/about/biopic.jpg";
 import { createClient } from "next-sanity";
+import { PortableText } from "@portabletext/react";
+import urlBuilder from "@sanity/image-url";
+import { getImageDimensions } from "@sanity/asset-utils";
+import { projectId } from "@/constants";
 
 const client = createClient({
-  projectId: "0jfaimql",
+  projectId,
   dataset: "production",
   useCdn: true,
 });
+
+// Barebones lazy-loaded image component
+const SampleImageComponent = ({ value, isInline }) => {
+  console.log("🚀 ~ file: About.tsx:18 ~ SampleImageComponent ~ value", value)
+  const { width, height } = getImageDimensions(value);
+  return (
+    <img
+      src={urlBuilder({ projectId, dataset: "production" })
+        .image(value)
+        .width(isInline ? 100 : 800)
+        .fit("max")
+        .auto("format")
+        .url()}
+      alt={value.alt || " "}
+      loading="lazy"
+      style={{
+        // Display alongside text if image appears inside a block text span
+        display: isInline ? "inline-block" : "block",
+
+        // Avoid jumping around with aspect-ratio CSS property
+        aspectRatio: width / height,
+      }}
+    />
+  );
+};
+
+const components = {
+  types: {
+    image: SampleImageComponent,
+    // Any other custom types you have in your content
+    // Examples: mapLocation, contactForm, code, featuredProjects, latestNews, etc.
+  },
+};
+
 const About = () => {
   const [text, setText] = useState("");
   useEffect(() => {
     (async () => {
-      const { name } = await client.fetch(`*[_type == "about"][0]{name}`);
-      setText(name);
+      const test = await client.fetch(`*[_type == "homepage"]`);
+      console.log("🚀 ~ file: About.tsx:19 ~ test", test);
+      console.log("🚀 ~ file: About.tsx:19 ~ test", test[0]?.myRichTextExample);
+      setText(test);
     })();
-  });
+  }, []);
 
   return (
     <Grid container spacing={2}>
@@ -34,7 +74,10 @@ const About = () => {
         <Image src={bioPhoto} alt="about" width={300} height={400} />
       </Grid>
       <Grid item xs={12} sm={12} md={6}>
-        <div>{text}</div>
+        <PortableText
+          value={text[0]?.myRichTextExample}
+          components={components}
+        />
       </Grid>
     </Grid>
   );
--- src/constants.tsx
+++ src/constants.tsx
@@ -2,5 +2,6 @@ const heroVideoUrl =
   "https://storage.googleapis.com/sacko-backend.appspot.com/YouCut_20211107_183543938.mp4";
 
 const calendyLink = "https://www.google.co.uk/";
+const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID
 
-export { heroVideoUrl, calendyLink };
+export { heroVideoUrl, calendyLink, projectId };
